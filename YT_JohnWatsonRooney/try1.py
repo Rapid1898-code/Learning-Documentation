b2a1d875ac1db1b22b506f42bd327536d216c1fb
Rapid1898 <rapid1898@gmail.com>
2025-01-09 17:34:16 +0100

    Upd

--- YT_JohnWatsonRooney/try1.py
+++ YT_JohnWatsonRooney/try1.py
@@ -1,2 +1,10 @@
 # Link: https://www.youtube.com/watch?v=N9tFdJ1_uvo
 # ProductLink: https://www.sorelfootwear.co.uk/c/men/snow-boots/iconic-snow-boots
+# JsonLink: https://www.sorelfootwear.co.uk/on/demandware.store/Sites-Sorel_UK-Site/en_GB/Product-Variation?dwvar_2084901_color=010&dwvar_2084901_size=12&pid=2084901&selection.size=12
+
+import requests
+from bs4 import BeautifulSoup
+
+def create_session()k:
+  session = requests.Session()
+  session.headers.update()
